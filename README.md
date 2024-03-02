2c49639a2a557204cfd70aaa30f9c7a06d5cda4e
Kyla Dawon Park <dawonpark@Kylas-MacBook-Pro.local>
2024-03-01 23:40:33 -0800

    prototype

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# dsc106-final-project
+# set-up
+
+Use the following code to setup:
+
+```bash
+npm install .
+npm run dev -- --open
+```
+
---
+++ src/components/Graph.svelte
@@ -0,0 +1,56 @@
+<!-- <script>
+  import { fly, draw } from 'svelte/transition';
+  import { tweened } from 'svelte/motion';
+  import { cubicOut, cubicInOut } from 'svelte/easing';
+  import { cities } from '../data/olympic.csv';
+
+  const CITY_COLUMN_INDEX = 0;
+
+  export let index, projection; // width, height,
+
+  const tweenOptions = {
+    delay: 0,
+    duration: 1000,
+    easing: cubicOut,
+  };
+
+  const tweenedX = tweened(
+    cities.features.map((city) => projection(city.geometry.coordinates)[0]),
+    tweenOptions
+  );
+
+  const tweenedY = tweened(
+    cities.features.map((city) => projection(city.geometry.coordinates)[1]),
+    tweenOptions
+  );
+
+  $: tweenedData = cities.features.map((city, i) => ({
+    x: $tweenedX[i],
+    y: $tweenedY[i],
+    city: city.properties[CITY_COLUMN_INDEX],
+  }));
+
+  $: uniqueCities = Array.from(new Set(tweenedData.map((data) => data.city)));
+
+  // Mark up only unique city values
+  $: markedUpCities = uniqueCities.map((city) => ({
+    city,
+    // Assuming you want to retain the first occurrence of the city's coordinates
+    x: tweenedData.find((data) => data.city === city).x,
+    y: tweenedData.find((data) => data.city === city).y,
+  }));
+
+  $: {
+    if (index === 1) {
+      tweenedX.set(
+        cities.features.map((city) => projection(city.geometry.coordinates)[0])
+      );
+      tweenedY.set(
+        cities.features.map((city) => projection(city.geometry.coordinates)[1])
+      );
+    }
+  }
+</script>
+
+<style>
+</style> -->
